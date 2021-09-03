3e70f696419589f0e45ba7d5e40f500c7fad2422
SimplyElvis <provictor.ie@gmail.com>
2021-09-03 13:32:30 +0100

    created data.js, deleted data.json

---
+++ public/scripts/data.js
@@ -0,0 +1,30 @@
+const data = [
+  {
+    id: 1,
+    title: "Bamboo Stand",
+    pledgeBid: "24",
+    desc: "You get an ergnomic stand made of natural bamboo. You've helped us lunch our promotional campaing, and you'll be added to a special Backer member list.",
+    remains: "101",
+  },
+  {
+    id: 2,
+    title: "Black Edition Stand",
+    pledgeBid: "56",
+    desc: "You get a Black Edition computer stand and a personal thank you. You'll be added to our Backer member list. Shipping is included",
+    remains: "73",
+  },
+  {
+    id: 3,
+    title: "Mahogany Special Edition",
+    pledgeBid: "200",
+    desc: "You get two Special Edition Mahogany stands, a Backer T-Shirt, and a personal thank you. You'll  be added to our Backer member list. Shippping is included",
+    remains: "0",
+  },
+  {
+    id: 4,
+    title: "Pledge Reward",
+    pledgeBid: "24",
+    desc: "You get an ergnomic stand made of natural bamboo. You've helped us lunch our promotional campaing, and you'll be added to a special Backer member list.",
+    remains: "5",
+  },
+];
